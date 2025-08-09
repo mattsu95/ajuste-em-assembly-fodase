a0d8af1758099c9d4bdc4cb05c196dee3c0abaff
Piterzin <124799280+IceyPiter@users.noreply.github.com>
2025-08-09 17:49:13 -0300

    Update sla.asm

--- sla.asm
+++ sla.asm
@@ -24,29 +24,29 @@ main:
     push rbp
     mov rbp, rsp
 
-    call r_arquivo
+    call r_arquivo  ; chama a função que abre e lê o arquivo
 
-    mov rax, 60
-    mov rdi, 0
+    mov rax, 60  ; chama a função que abre e lê o arquivo
+    mov rdi, 0 ; exit code 0
     syscall
 
 r_arquivo:
     push rbp
     mov rbp, rsp
 
-    push r12
-    push r13
+    push r12 ; salvar r12 (callee-saved)
+    push r13 ; salvar r13 (callee-saved)
 
-    sub rsp, 32
+    sub rsp, 32 ; salvar r12 (callee-saved)
     ;stack frame caga com a pilha ent tem que pegar o parâmetro pelo rdi
-    cmp rdi, 2 
-    jl erro
+    cmp rdi, 2  
+    jl erro ; exige argc >= 2 (programa + 1 argumento)
 
     mov rdx, [rsi + 8]  ;rdx = filename
     mov rdi, rdx        ;passa como parâmetro
     lea rsi, [rmode]    ;modo de leitura
     xor rax, rax
-    call fopen
+    call fopen ; FILE *fopen(const char *filename, const char *mode)
 
     mov r12, rax        ;r12 = *file
     xor r13, r13        ;r13 = índice
@@ -55,42 +55,43 @@ r_arquivo:
 
 laco_r:
     xor rax, rax
-    mov rdi, r12
-    lea rsi, [strctr]
+    mov rdi, r12 ; primeiro parâmetro de fscanf: FILE*
+    lea rsi, [strctr] ; formato "%f %f"
     lea rdx, [x + r13*4]    ;endereço de x[i]
     lea rcx, [y + r13*4]    ;endereço de y[i]
-    call fscanf
+    call fscanf ; fscanf(file, "%f %f", &x[i], &y[i])
 
     cmp rax, 2              ;rax = qtd de valores lidos
     jne  fim_r               ;se for menor que 2, na teoria leu todos
 
-    movss xmm0, dword [x + r13*4]
-    cvtss2sd xmm0, xmm0
+    movss xmm0, dword [x + r13*4] ; carrega float x[i] em xmm0
+    cvtss2sd xmm0, xmm0 ; converte float->double (printf espera double nos xmm)
     movss xmm1, dword [y + r13*4]
     cvtss2sd xmm1, xmm1
-    mov rax, 2
-    lea rdi, [pdstrctr]
-    call printf
 
-    inc r13
+    mov rax, 2 ; <-- intenção: informar nº de XMM usados
+    lea rdi, [pdstrctr] ; fmt para printf: "Lidos: %f %f"
+    call printf ; fmt para printf: "Lidos: %f %f"
+
+    inc r13 ; i++
     cmp r13, 64
-    jl laco_r
+    jl laco_r ; i++
 
 
 fim_r:
-    mov rdi, r12
-    call fclose
+    mov rdi, r12 ; FILE* em r12
+    call fclose ; fecha o arquivo
     jmp fim
 
 erro:
-    lea rdi, [pstrctr]
+    lea rdi, [pstrctr] ; mensagem de erro
     xor rax, rax
-    call printf
+    call printf ; imprime erro
 
 fim:
-    add rsp, 32
-    pop r13
-    pop r12
+    add rsp, 32 ; desfaz sub rsp,32
+    pop r13 ; restaura r13
+    pop r12 ; restaura r12
     mov rsp, rbp
     pop rbp
     ret
